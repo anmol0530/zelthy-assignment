28cdbc08d0edbb92e113974cab025f4f79d71679
Anmol Julka <julka0530@gmail.com>
2021-07-25 11:30:45 +0530

    Create initial react app using npx creat-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Zelthy Assignment
+      </header>
+    </div>
+  );
+}
+
+export default App;
